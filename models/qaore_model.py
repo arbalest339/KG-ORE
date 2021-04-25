5b0627793171b5a0e63a7ed122f0c4c186675fcc
arbalest339 <17801188346@163.com>
2021-04-25 10:05:24 +0800

    origin qa

--- models/qaore_model.py
+++ models/qaore_model.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-03-08 08:39:21
-LastEditTime: 2021-04-20 15:04:18
+LastEditTime: 2021-04-25 09:18:54
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /code_for_project/models/ore_model.py
@@ -23,7 +23,8 @@ class OREModel(nn.Module):
         self.features = flags.features
 
         # local bert
-        self.bert = BertModel.from_pretrained(flags.pretrained, config=bertconfig)
+        self.bert = BertForQuestionAnswering.from_pretrained(
+            flags.pretrained, config=bertconfig)
 
         # feature emb
         self.qa_hidden = bertconfig.hidden_size
@@ -41,46 +42,52 @@ class OREModel(nn.Module):
         self.sofmax = torch.nn.Softmax(dim=-1)
 
     def forward(self, input_ids, mask, type_ids, start_ids, end_ids):
+        outputs = self.bert(input_ids=input_ids, attention_mask=mask,
+                            token_type_ids=type_ids, start_positions=start_ids, end_positions=end_ids)
         # BERT's last hidden layer
-        outputs = self.bert(input_ids, attention_mask=mask, token_type_ids=type_ids)
-        sequence_output = outputs[0]
+        # outputs = self.bert(input_ids, attention_mask=mask, token_type_ids=type_ids)
+        # sequence_output = outputs[0]
 
-        # feature concat
-        # if "desc" in self.features:
-        #     descEmb = self.desc(desc)
-        # if "exrest" in self.features:
-        #     exrestEmb = self.exrest(exrest)
-        # if "kbRel" in self.features:
-        #     kbRelEmb = self.kbRel(kbRel)
-        # sequence_output = torch.concat()
+        # # feature concat
+        # # if "desc" in self.features:
+        # #     descEmb = self.desc(desc)
+        # # if "exrest" in self.features:
+        # #     exrestEmb = self.exrest(exrest)
+        # # if "kbRel" in self.features:
+        # #     kbRelEmb = self.kbRel(kbRel)
+        # # sequence_output = torch.concat()
 
-        # fc layer
-        logits = self.qa_outputs(sequence_output)
-        start_logits, end_logits = logits.split(1, dim=-1)
-        start_logits = start_logits.squeeze(-1)
-        end_logits = end_logits.squeeze(-1)
+        # # fc layer
+        # logits = self.qa_outputs(sequence_output)
+        # start_logits, end_logits = logits.split(1, dim=-1)
+        # start_logits = start_logits.squeeze(-1)
+        # end_logits = end_logits.squeeze(-1)
 
-        total_loss = None
-        if start_ids is not None and end_ids is not None:
-            # If we are on multi-GPU, split add a dimension
-            if len(start_ids.size()) > 1:
-                start_ids = start_ids.squeeze(-1)
-            if len(end_ids.size()) > 1:
-                end_ids = end_ids.squeeze(-1)
-            # sometimes the start/end positions are outside our model inputs, we ignore these terms
-            ignored_index = start_logits.size(1)
-            start_ids.clamp_(0, ignored_index)
-            end_ids.clamp_(0, ignored_index)
+        # total_loss = None
+        # if start_ids is not None and end_ids is not None:
+        #     # If we are on multi-GPU, split add a dimension
+        #     if len(start_ids.size()) > 1:
+        #         start_ids = start_ids.squeeze(-1)
+        #     if len(end_ids.size()) > 1:
+        #         end_ids = end_ids.squeeze(-1)
+        #     # sometimes the start/end positions are outside our model inputs, we ignore these terms
+        #     ignored_index = start_logits.size(1)
+        #     start_ids.clamp_(0, ignored_index)
+        #     end_ids.clamp_(0, ignored_index)
 
-            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
-            start_loss = loss_fct(start_logits, start_ids)
-            end_loss = loss_fct(end_logits, end_ids)
-            total_loss = (start_loss + end_loss) / 2
+        #     loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+        #     start_loss = loss_fct(start_logits, start_ids)
+        #     end_loss = loss_fct(end_logits, end_ids)
+        #     total_loss = (start_loss + end_loss) / 2
 
+        total_loss = outputs.loss
+        start_logits = outputs.start_logits
+        end_logits = outputs.end_logits
         _, pred_s = torch.max(self.sofmax(start_logits), dim=-1)
         _, end_s = torch.max(self.sofmax(end_logits), dim=-1)
         output = (pred_s, end_s)     # + outputs[2:]
-        return ((total_loss,) + output) if total_loss is not None else output
+        # return ((total_loss,) + output) if total_loss is not None else output
+        return (total_loss, pred_s, end_s)
 
     def decode(self, token, pos, mask):
         # BERT's last hidden layer
